e1f03be5ad2d5d212f758f841909274aec5d2b7c
abbydoag <86386562+abbydoag@users.noreply.github.com>
2025-02-21 21:12:17 -0600

    Correciones

--- Ejercicio2B.c
+++ Ejercicio2B.c
@@ -8,35 +8,32 @@ int main() {
     clock_t start, end;
     double elapsed_time;
 
-    // Captura el tiempo antes del primer fork()
     start = clock();
 
-    pid_hijo = fork(); // Primer fork: crea el proceso hijo
+    pid_hijo = fork(); //proceso hijo
 
-    if (pid_hijo == 0) { // Código del hijo
-        pid_nieto = fork(); // Segundo fork: crea el proceso nieto
+    if (pid_hijo == 0) { // Código hijo
+        pid_nieto = fork(); // proceso nieto
 
-        if (pid_nieto == 0) { // Código del nieto
-            pid_bisnieto = fork(); // Tercer fork: crea el proceso bisnieto
+        if (pid_nieto == 0) { //nieto
+            pid_bisnieto = fork(); // proceso bisnieto (?
 
-            if (pid_bisnieto == 0) { // Código del bisnieto
-                for (int i = 0; i < 1000000; i++); // Ciclo del bisnieto
-            } else { // Código del nieto
-                wait(NULL); // Espera a que termine el bisnieto
-                for (int i = 0; i < 1000000; i++); // Ciclo del nieto
+            if (pid_bisnieto == 0) { 
+                for (int i = 0; i < 1000000; i++);
+            } else { 
+                wait(NULL); //espera a que termine
+                for (int i = 0; i < 1000000; i++); 
             }
-        } else { // Código del hijo
-            wait(NULL); // Espera a que termine el nieto
-            for (int i = 0; i < 1000000; i++); // Ciclo del hijo
+        } else {
+            wait(NULL);
+            for (int i = 0; i < 1000000; i++); 
         }
-    } else { // Código del proceso padre
-        wait(NULL); // Espera a que termine el hijo
-        end = clock(); // Captura el tiempo después de que termine el hijo
+    } else {
+        wait(NULL);
+        end = clock();
 
-        // Cálculo del tiempo transcurrido en segundos
         elapsed_time = (double)(end - start) / CLOCKS_PER_SEC;
         printf("Tiempo transcurrido: %f segundos\n", elapsed_time);
     }
-
     return 0;
 }
